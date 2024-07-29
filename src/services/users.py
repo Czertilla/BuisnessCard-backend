a27e508b52b6a82ef92d118370611e22ecd09287
Czertilla <blaztermapskorsak@gmail.com>
2024-07-29 16:26:42 +0300

    fix(UserService) modify check_username method

--- src/services/users.py
+++ src/services/users.py
@@ -2,26 +2,23 @@ from logging import getLogger
 from typing import override
 from schemas.auth import UserUpdate
 from utils.absract.service import BaseService
+from pydantic import validate_call
 from loguru import logger
 
 # logger = getLogger(__name__)
 
 
 class UserService(BaseService):
+    @validate_call
     async def check_username(self, value: str | UserUpdate) -> bool:
         logger.trace(f"checking username: {value=}")
+        if isinstance(value, str):
+            logger.debug(f"{value=} is username")
+        elif isinstance(value, UserUpdate):
+            logger.debug(f"{value=} is user")
+            value: str = value.username
+        else:
+            logger.warning(
+                f"value is not valid instance: {value.__class__}")
         async with self.uow:
-            if isinstance(value, str):
-                logger.debug(f"{value=} is username")
-                return await self.uow.users.check_username(value)
-            elif isinstance(value, UserUpdate):
-                logger.debug(f"{value=} is user")
-                if await self.uow.users.check_username(value.username):
-                    return value.username != getattr(
-                        (await self.uow.users.get_by_email(value.email)),
-                        "username",
-                        None
-                    )
-            else:
-                logger.warning(
-                    f"value is not valid instance: {value.__class__}")
+            return await self.uow.users.check_username(value)
